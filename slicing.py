a23d49a6049dcd6e5de8ed5f9413d3cafa866055
VijayaLaxmi20 <100471983+VijayaLaxmi20@users.noreply.github.com>
2022-03-04 21:43:08 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,30 @@
+s='saimohan'
+print(s[ : : ])
+print(s[ 0: :1 ])
+print(s[ 1: : 2])
+print(s[ 3:4 :2 ])
+print(s[ 1:2:3 ])
+print(s[ 2:4 :6 ])
+print(s[ 1:3 :5 ])
+print(s[ 2:3 :4 ])
+print(s[ 1:3 :5 ])
+m='siddubhai'
+print(m[ : :-1])
+print(m[ : :-2])
+print(m[ : :-3])
+print(m[ : :-4])
+print(m[ : :-5])
+print(m[ : :-6])
+print(m[ : :-7])
+print(m[ : :-8])
+print(m[ -2: :-1])
+print(m[ -3:-2 :-1])
+# print(m[-4:-3:-5])
+# a='chantiakki'
+# print(a,type(a))
+# print(a[0])
+# a[0]=c
+name='manishankar'
+print(name.capitalize())
+name='akhil kumar'
+print(name.capitalize())
