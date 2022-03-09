7f8e2dc2ba708c9ad4cb1f145750f632483e2bd0
VijayaLaxmi20 <100471983+VijayaLaxmi20@users.noreply.github.com>
2022-03-09 12:15:12 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,21 @@
+g='   '
+print(g.isspace())
+s=' aurora degree college'
+print(s.index('r'))
+print(s.index('r',5))
+print(s.index('r',11))
+print(s.index('a'))
+print(s.index('e'))
+print(s.index('c'))
+print(s.index('g'))
+t='mani shankar'
+print(t.index('m'))
+print(t.index('s'))
+print(t.find('a'))
+print(t.find('r'))
+print(t.find('n'))
+print(t.find('q'))
+print(t.count('q'))
+print(t.count('i'))
+print(t.count('k'))
+print(t[0])
